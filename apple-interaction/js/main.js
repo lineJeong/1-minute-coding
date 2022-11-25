a9d2682f63bd2cd040acc0343fb628244291192c
lineJeong <line.jeong32@gmail.com>
2022-11-26 01:07:08 +0900

    스크롤에 따른 캔버스 포지션 변경

--- apple-interaction/js/main.js
+++ apple-interaction/js/main.js
@@ -460,6 +460,7 @@
 
       case 3:
         // console.log("3 play");
+        let step = 0;
         // 가로/세로 모두 꽉 차게 하기 위해 여기서 세팅(계산 필요)
         const widthRatio = window.innerWidth / objs.canvas.width;
         const heightRatio = window.innerHeight / objs.canvas.height;
@@ -523,6 +524,18 @@
           parseInt(whiteRectWidth),
           objs.canvas.height
         );
+
+        if (scrollRatio < values.rect1X[2].end) {
+          step = 1;
+          objs.canvas.classList.remove("sticky");
+        } else {
+          step = 2;
+          // 이미지 블랜드
+          objs.canvas.classList.add("sticky");
+          objs.canvas.style.top = `${
+            -(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2
+          }px`;
+        }
 
         break;
     }
